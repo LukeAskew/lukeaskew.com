b4e2fabea86553173ccd555fc920e32e0c128cd9
Luke Askew <luke@dynamit.com>
2016-06-01 22:36:17 -0400

    Add react-helmet

--- app/views/App.jsx
+++ app/views/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
+        <Helmet
+          title={'Luke Askew'}
+          meta={[
+            { name: 'description', content: 'A developer\'s thoughts about front-end architecture, website design, and workflow.' }
+          ]} />
         {this.props.children}
       </div>
     )
