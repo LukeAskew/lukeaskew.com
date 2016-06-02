8f7b30173da557613c29a4bceb7ccba6cf14a3d4
Luke Askew <luke@dynamit.com>
2016-06-01 23:49:48 -0400

    add GA code

--- app/views/Layout.jsx
+++ app/views/Layout.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 class Layout extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.GA = `
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-22360854-1', 'auto');
+      ga('send', 'pageview');
+    `;
+  }
+
   render() {
     return (
       <html lang={'en'}>
@@ -10,6 +23,7 @@ class Layout extends React.Component {
           <meta name={'viewport'} content={'width=device-width, initial-scale=1'} />
           <title>{'Title'}</title>
           {(process.env.NODE_ENV === 'production') ? <link rel={'stylesheet'} href={'/main.css'} /> : <link />}
+          <script dangerouslySetInnerHTML={{__html: this.GA }}></script>
         </head>
         <body>
           <div id={'app'}>
